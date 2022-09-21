f0a57b9233985b66318c0dc21a42dffdec02be1f
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-09-21 21:17:20 +0000

    Added code to problem_solving.py

---
+++ problem_solving.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Kaitlyn Ip
+# Created on: Sep 2022
+# This program calculates the area and perimeter of a rectangle
+
+import math
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a circle has a radius of 15 mm: ")
+    print("")
+    print("Area is {} mm².".format(math.pi * 15 ** 2))
+    print("Perimeter is {} mm.".format(2 * math.pi * 15))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
